6690b7685db6d7a5411f97c15ab2f4b525ae1d4f
Einshine || Shine <theeinshine3301@gmail.com>
2026-06-18 17:11:18 +0530

    Update main.go (#1)

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Shine's Service v3 is running")
+		fmt.Fprintln(w, "Shine's Service v4 is running")
 	})
 
 	fmt.Println("Listening on :8080")
